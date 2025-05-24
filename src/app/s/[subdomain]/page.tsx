33b88d7aae1ee03dd3ecf7f4e341b56870613e1d
fikertag <149786264+fikertag@users.noreply.github.com>
2025-05-24 09:56:26 +0300

    prams from next.js docs

--- src/app/s/[subdomain]/page.tsx
+++ src/app/s/[subdomain]/page.tsx
@@ -1,14 +1,8 @@
-import { Metadata } from 'next';
-
-export const generateMetadata = ({ params }: { params: { subdomain: string } }): Metadata => ({
-  title: `Welcome, ${params.subdomain}`
-});
-
-export default function SubdomainPage(props: { params: { subdomain: string } }) {
-  return (
-    <main className="flex flex-col items-center justify-center min-h-screen">
-      <h1 className="text-4xl font-bold">Hello, {props.params.subdomain}!</h1>
-      <p className="text-lg text-gray-600 mt-2">This is your custom subdomain page.</p>
-    </main>
-  );
+export default async function Page({
+  params,
+}: {
+  params: Promise<{ subdomain: string }>
+}) {
+  const { subdomain } = await params
+  return <div>welcome : {subdomain}</div>
 }
