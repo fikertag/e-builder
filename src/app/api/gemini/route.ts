df920ab5b5d1bcf8c0991f9bf0f785709f4544ee
fikertag <149786264+fikertag@users.noreply.github.com>
2025-06-14 19:40:42 +0300

    gemini pro model integrated

---
+++ src/app/api/gemini/route.ts
@@ -0,0 +1,18 @@
+import { GoogleGenerativeAI } from "@google/generative-ai";
+
+const geminiAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY!);
+
+export async function POST(request: Request) {
+  try {
+    const model = geminiAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+    const { prompt } = await request.json(); 
+
+    const result = await model.generateContent(prompt);
+    const response = await result.response;
+    const text = response.text();
+    return Response.json({ message: text });
+  } catch (error) {
+    console.error("Error generating content:", error);
+    return Response.json({ message: error });
+  }
+}
